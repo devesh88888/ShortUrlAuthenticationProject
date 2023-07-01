4effab1f959473bda6b3b5766bf449cc0b1a75d8
Devesh <rakhejadevesh@gmail.com>
2023-07-01 19:31:43 +0530

    stateless authentication using jwt

--- service/auth.js
+++ service/auth.js
@@ -1,11 +1,24 @@
-const sessionIdToUserMap = new Map();
-
-function setUser(id, user) {
-  sessionIdToUserMap.set(id, user);
+const jwt = require("jsonwebtoken");
+//const jwt = require("json-web-token");
+const secret = "Dev#5%%uiu";
+function setUser(user) {
+  return jwt.sign(
+    {
+      _id: user._id,
+      email: user.email,
+    },
+    secret
+  );
 }
 
-function getUser(id) {
-  return sessionIdToUserMap.get(id);
+function getUser(token) {
+  if (!token) return null;
+
+  try {
+    return jwt.verify(token, secret);
+  } catch (error) {
+    return null;
+  }
 }
 
 module.exports = {
